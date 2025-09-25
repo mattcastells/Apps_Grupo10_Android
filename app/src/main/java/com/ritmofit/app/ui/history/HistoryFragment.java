d7dec3d664e79ad6b5715e31cbd7ca64d96ca486
Ramiro Kalinoski NB <ramikalinoski@gmail.com>
2025-09-25 18:35:38 -0300

    Creacion de xml y java básico para el historial / Modificacion del navgraph y del menu inferior para incluir acceso al historial / Se crea icono para historial en el directorio drawable

---
+++ app/src/main/java/com/ritmofit/app/ui/history/HistoryFragment.java
@@ -0,0 +1,17 @@
+package com.ritmofit.app.ui.history;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import com.ritmofit.app.R;
+
+public class HistoryFragment extends Fragment {
+    @Nullable @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_history, container, false);
+    }
+}
